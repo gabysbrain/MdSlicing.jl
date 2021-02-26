f8aaf3108fab208512015988e130585242221894
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-02-26 15:29:28 +0000

    slice function type annotation and proper handling

--- src/hyperslice.jl
+++ src/hyperslice.jl
@@ -1,6 +1,6 @@
-#include("types.jl")
+# include("types.jl")
 include("util.jl")
-#include("IntersectTest.jl")
+# include("IntersectTest.jl")
 
 using Sobol: SobolSeq, next!
 using JSON
@@ -9,8 +9,8 @@ function hyperslice(mesh::ConvexMesh, n::UInt32=UInt32(50))
   dims = size(mesh)[2]
 
   # Generate the focus points
-  lbs = [b[1] for b=mesh.problemSpec.vals]
-  ubs = [b[2] for b=mesh.problemSpec.vals]
+  lbs = [b[1] for b = mesh.problemSpec.vals]
+  ubs = [b[2] for b = mesh.problemSpec.vals]
   s = SobolSeq(lbs, ubs)
   fps = collect([next!(s) for _ = 1:n])
 
@@ -31,16 +31,16 @@ function sliceDims(mesh::ConvexMesh, fp::PointND)
   slices = []
   for dd in dimComb(UInt64(size(mesh)[2]))
     ss = slice(mesh, fp, dd[1], dd[2])
-    hss = [HypersliceSegment(fp, simp, dd[1], dd[2], s.p1d1, s.p1d2, s.p2d1, s.p2d2) for (simp,s) in ss]
+    hss = [HypersliceSegment(fp, simp, dd[1], dd[2], s.p1d1, s.p1d2, s.p2d1, s.p2d2) for (simp, s) in ss]
     append!(slices, hss)
   end
   return slices
 end
 
-function slice(mesh::ConvexMesh, fp::PointND, d1::Dim, d2::Dim)
+function slice(mesh::ConvexMesh, fp::PointND, d1::Dim, d2::Dim)::Vector{Tuple{Simplex,Intersect2D}}
   slices = Tuple{Simplex,Intersect2D}[]
   for s in mesh
-    ts = [(s,t) for t in simplexPointIntersection(s, fp, d1, d2)]
+    ts = [(s, t) for t in simplexPointIntersection(s, fp, d1, d2)]
     append!(slices, ts)
   end
   return slices
